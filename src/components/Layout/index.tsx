799057e3078ff67326d134aef9c1c7ae677b5d37
jjh4450 <jjh4450git@gmail.com>
2025-09-19 04:41:39 +0900

    feat: 레이아웃 및 스타일 컴포넌트 수정 및 추가

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/components/Header";
+import { layoutContainer } from "./styles.css";
 
 function Layout() {
   return (
-    <div>
+    <div className={layoutContainer}>
       <Header />
       <Outlet />
     </div>
